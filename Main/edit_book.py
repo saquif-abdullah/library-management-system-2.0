a2732f0b648b8b98cb4f052b687c1f6ed3fb0eb1
Saquif Abdullah <saquif.abdullah@gmail.com>
2024-12-16 18:17:42 +0600

    Edit Completed

--- Main/edit_book.py
+++ Main/edit_book.py
@@ -1,3 +1,42 @@
 
-def editBook(file):
-    print("Edit")
+import main
+import my_tools
+import read_books
+import write_books
+
+def editBook(file_name):
+    my_tools.Clear()
+    print("\n_____________Edit Book__________\n\n")
+    
+    ob = main.LibraryManagementSystem()
+
+    print("    1. Search book for Edit")
+    print("    2. View all books for choosing book to Edit")
+
+    key = int(input("\n    Choose an option : "))
+
+    if key == 1: 
+        ob.searchBook()
+    elif key == 2:
+        ob.viewBooks()
+    else:
+        print("    Envalid input.")
+    
+    to_edit = int(input("\n\n    Enter ID of the book : "))
+
+    books = read_books.readBooks(file_name)
+    
+    print("\n\n    Old Data")
+    for key, value in books[to_edit].items():
+        print(f"      {key} : {value}")
+    
+    keys = books[to_edit].keys()
+    print("\n    Enter new Data.")
+    for key in keys:
+        if key == "isbn" or key == "time":
+            continue
+        books[to_edit][key] = input(f"      {key} : ")
+    
+    
+    write_books.writeBooks(file_name, books)
+    print("\n    Book Edited Successfully")
